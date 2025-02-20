c6ccbc36fba0bf281f162080fee6e03c71e2a4b6
anhchien2509 <chienlaps38508@gmail.com>
2025-02-20 16:01:55 +0700

    Chien
    Code Play, Quit, Code Setting and Panal Setting

---
+++ Assets/Chien/Scripts/SettingsMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject settingsPanel; // Tham chiếu đến Settings Panel
+
+    public void ToggleSettings()
+    {
+        settingsPanel.SetActive(!settingsPanel.activeSelf); // Bật/tắt panel
+    }
+}
