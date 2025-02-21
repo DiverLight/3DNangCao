ca07edb653f2f05b85d70a581b9cd5481772cc8c
anhchien2509 <chienlaps38508@gmail.com>
2025-02-21 20:49:45 +0700

    Chien
    Them Setting UI cho map

---
+++ Assets/Chien/Scripts/MiniSettingsManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Import SceneManager
+
+public class MiniSettingsManager : MonoBehaviour
+{
+    public GameObject miniSettingsPanel;
+
+    void Start()
+    {
+        // Ẩn panel lúc đầu
+        miniSettingsPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Nhấn ESC để đóng MiniSettingsPanel nếu nó đang mở
+        if (Input.GetKeyDown(KeyCode.Escape) && miniSettingsPanel.activeSelf)
+        {
+            ReturnToGame();
+        }
+    }
+
+    public void ToggleMiniSettings()
+    {
+        bool isActive = !miniSettingsPanel.activeSelf;
+        miniSettingsPanel.SetActive(isActive);
+
+        // Dừng game khi mở panel
+        if (isActive)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void ReturnToGame()
+    {
+        miniSettingsPanel.SetActive(false); // Ẩn panel
+        Time.timeScale = 1f; // Tiếp tục game
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f; // Đảm bảo thời gian chạy bình thường trước khi load scene mới
+        SceneManager.LoadScene("Menu"); // Thay "Menu" bằng tên Scene Menu của bạn
+    }
+}
+
